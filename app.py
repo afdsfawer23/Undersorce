00d29cc75570575483d299fc6b4f2255e145df79
afdsfawer23 <nickpoi344@gmail.com>
2026-01-18 12:21:46 -0500

    Configure app to listen on all interfaces

--- app.py
+++ app.py
@@ -795,4 +795,4 @@ def like_book(book_id):
     return jsonify({'likes': len(likes[book_id])})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
